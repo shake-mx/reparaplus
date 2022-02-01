bfeb9c2998996dc515ed03bb87b0e8a605d57ab5
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2022-02-01 01:32:12 -0600

    se arreglan formas de pago

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,80 @@ btn.on('click', function(e) {
 
 
 
+
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+
+
+if(window.matchMedia("(min-width: 1400px)").matches){
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+      
+    } else {
+      document.getElementById("navbar").style.top = "120px";
+    }
+  }
+}else if(window.matchMedia("(min-width: 1200px)").matches){ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+    } else {
+      document.getElementById("navbar").style.top = "140px";
+
+    }
+  }
+}else if(window.matchMedia("(min-width: 992px)").matches){ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+      
+    } else {
+      document.getElementById("navbar").style.top = "150px";
+    }
+  }
+}else if(window.matchMedia("(min-width: 768px)").matches){ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+    } else {
+      document.getElementById("navbar").style.top = "220px";
+    }
+  }
+}else if(window.matchMedia("(min-width: 576px)").matches){ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+    } else {
+      document.getElementById("navbar").style.top = "120px";
+    }
+  }
+}else{
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      
+    } else {
+      document.getElementById("navbar").style.top = "150px";
+    }
+  }
+}
+
+
+
+window.onload = function() {
+  var myModalGracias = new bootstrap.Modal(
+    document.getElementById("modalGracias"),
+    {}
+  );
+  myModalGracias.show();
+
+
+};
